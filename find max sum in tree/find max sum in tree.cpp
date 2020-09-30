1cd05f09e8b937d169f12d9243feb5eae1183f92
compiletimeterror <56031091+compiletimeterror@users.noreply.github.com>
2020-09-30 23:47:38 +0530

    Update find max sum in tree.cpp

--- find max sum in tree/find max sum in tree.cpp
+++ find max sum in tree/find max sum in tree.cpp
@@ -6,18 +6,5 @@ int findMaxSum(node* root, int& ans){
         return 0;
     if(root->left == NULL and root->right == NULL)
         return root->data;
-    int l = findMaxSum(root->left, ans);
-    int r = findMaxSum(root->right, ans);
-    int val=root->data, maxval=INT_MIN;
-    if(root->left != NULL){
-        maxval = max(maxval, l);
-        val += l;
-    }
-    if(root->right != NULL){
-        maxval = max(maxval, r);
-        val += r;
-    }
-    if(root->left != NULL and root->right != NULL)
-        ans = max(ans, val);
-    return root->data+maxval;
+   //complete the code
 }
