0ef78ebf1f9ea33b7b29c90cb0e3f14bca502a52
compiletimeterror <56031091+compiletimeterror@users.noreply.github.com>
2020-09-30 23:47:02 +0530

    Update bst from postorder traversal.cpp

--- bst from postorder traversal/bst from postorder traversal.cpp
+++ bst from postorder traversal/bst from postorder traversal.cpp
@@ -4,23 +4,7 @@ node *constructBSTFromPostOrder(int post[], int size){
     int i=size-1;
     node* root = new node(post[i--]);
     node* ptr = root;
-    stack<node*> s;
-    s.push(ptr);
-    while(i >= 0){
-        while(i>=0 and post[i] > ptr->data){
-            ptr->right = new node(post[i--]);
-            ptr = ptr->right;
-            s.push(ptr);
-        }
-        while(!s.empty() and s.top()->data > post[i]){
-            ptr = s.top();
-            s.pop();
-        }
-        if(i >= 0)
-            ptr->left = new node(post[i--]);
-        ptr = ptr->left;
-        s.push(ptr);
-    }
+    //complete the code
     return root;    
 }
 
