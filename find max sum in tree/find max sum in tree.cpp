0d7d1eec5867090208766074ec7459deea0703ac
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-10-01 00:29:47 +0530

    Update find max sum in tree.cpp

--- find max sum in tree/find max sum in tree.cpp
+++ find max sum in tree/find max sum in tree.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int findMaxSum(node* root, int& ans){
-    if(root == NULL)
+struct Node
+{
+    int val;
+    Node *left;
+    Node *right;
+    Node() : val(0), left(nullptr), right(nullptr) {}
+    Node(int x) : val(x), left(nullptr), right(nullptr) {}
+    Node(int x, Node *left, Node *right) : val(x), left(left), right(right) {}
+};
+
+int ans=INT_MIN;
+int findMaxSum(Node* root,int &ans)
+{
+    if(root==NULL)
         return 0;
-    if(root->left == NULL and root->right == NULL)
-        return root->data;
-   //complete the code
+    int l=max(findMaxSum(root->left,ans),0);
+    int r=max(findMaxSum(root->right,ans),0);
+    ans=max(ans,root->val+l+r);
+    return root->val+max(l,r);
 }
