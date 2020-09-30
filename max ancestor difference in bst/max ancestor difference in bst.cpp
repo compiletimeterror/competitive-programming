6c39cb73f56ed8a5ff03eca2b0e4ba683580baeb
compiletimeterror <56031091+compiletimeterror@users.noreply.github.com>
2020-09-30 23:48:01 +0530

    Update max ancestor difference in bst.cpp

--- max ancestor difference in bst/max ancestor difference in bst.cpp
+++ max ancestor difference in bst/max ancestor difference in bst.cpp
@@ -6,18 +6,5 @@ pair<int, int> maxAncestorDiff(node* root, int& ans){
         return {0, 0};
     if(root->left == NULL and root->right == NULL)
         return {root->data, root->data};
-    pair<int, int> l = maxAncestorDiff(root->left, ans);
-    pair<int, int> r = maxAncestorDiff(root->right, ans);
-    int minval=root->data, maxval=root->data;
-    if(root->left != NULL){
-        minval = min(minval, l.first);
-        maxval = max(maxval, l.second);
-        ans = max(ans, max(abs(root->data-l.first), abs(root->data-l.second)));
-    }
-    if(root->right != NULL){
-        minval = min(minval, r.first);
-        maxval = max(maxval, r.second);
-        ans = max(ans, max(abs(root->data-r.first), abs(root->data-r.second)));
-    }
-    return {minval, maxval};
+    //complete the code
 }
